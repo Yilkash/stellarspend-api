27dc590c4bbdd72e6d928011573fe696542cb6cf
nafiuishaaq <nafiuig@gmail.com>
2026-03-02 16:54:26 +0100

    implemented the service

--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import {
   ApiTags,
   ApiOperation,
   ApiResponse,
   ApiBody,
   ApiQuery,
+  ApiBearerAuth,
 } from '@nestjs/swagger';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { QueryTransactionsDto } from './dto/query-transactions.dto';
 import { TransactionsService } from './transactions.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { AccountStatusGuard } from '../../common/guards/account-status.guard';
 
 @ApiTags('transactions')
+@ApiBearerAuth()
 @Controller('transactions')
+@UseGuards(JwtAuthGuard, AccountStatusGuard)
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
